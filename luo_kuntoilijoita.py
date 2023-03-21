c6b6482565e61d1b25d8561554e9c702d657fd20
HansumaeCecilia <48033@edu.raseko.fi>
2023-03-21 13:14:21 +0200

    Cleaned up code

--- luo_kuntoilijoita.py
+++ luo_kuntoilijoita.py
@@ -2,49 +2,73 @@
 # -------------------------------------------------------
 
 # LIBRARIES AND MODULES
+import json
 import kuntoilija
 import questions
-
 
 
 # Ask a question and convert the answer to float
 
 
 # Enter information about an athlete
-name = input('Anna nimesi: ')
+name = input('Nimi: ')
+date_of_weighing = input('Punnituspäivä (vvvv-kk-pp): ')
 
-# Ask details about him/her
-question = questions.Question('Anna painosi (kg): ')
-weight = question.ask_user_float(True)[0]
-question = questions.Question('Anna pituutesi (cm): ')
-height = question.ask_user_float(True)[0]
-question = questions.Question('Anna ikäsi: ')
-age = question.ask_user_integer(True)[0]
-question = questions.Question('Sukupuoli: mies(1), nainen(0): ')
-gender = question.ask_user_integer(True)[0]
-question = questions.Question('Anna niskasi ympärysmitta: ')
-neck = question.ask_user_float(True)[0]
-question = questions.Question('Anna vyötärösi ympärysmitta: ')
-waist = question.ask_user_float(True)[0]
+# Ask details about her/him
+
+weight = questions.Question.ask_user_float(
+    'Kuinka paljon painat (kg): ', True)[0]
+height = questions.Question.ask_user_float('Kuinka pitkä olet (cm): ', True)[0]
+age = questions.Question.ask_user_integer('Kuinka vanha olet: ', True)[0]
+gender = questions.Question.ask_user_integer(
+    'Sukupuoli 1 mies, 0 nainen: ', True)[0]
+neck = questions.Question.ask_user_float(
+    'Mikä on kaulanympäryksesi (cm): ', True)[0]
+waist = questions.Question.ask_user_float(
+    'Mikä on vyötärönympäryksesi: ', True)[0]
+
+# If woman ask circumference of her hips
 if gender == 0:
-    question = questions.Question('Lantiosi ympärysmitta: ')
-    hips = question.ask_user_float(True)[0]
+    hips = questions.Question.ask_user_float(
+        'Mikä on lantionympäryksesi: ', True)[0]
 
-# Create an athlete object from Kuntoilija class
-athlete = kuntoilija.Kuntoilija(name, height, weight, age, gender)
+# Create an athelete object from Kuntoilija class
+athlete = kuntoilija.Kuntoilija(
+    name, height, weight, age, gender, date_of_weighing)
 
-#Print some information about the athlete
-text_to_show = f'Terve {athlete.nimi}, painoindeksisi tänään on: {athlete.bmi}'
+# Print some information about the athlete
+text_to_show = f'Terve {athlete.nimi}, painoindeksisi tänään on {athlete.bmi}'
 print(text_to_show)
 fat_percentage = athlete.rasvaprosentti()
+
+# If male use usa_rasvaprosentti_mies method
 if gender == 1:
     usa_fat_percentage = athlete.usa_rasvaprosentti_mies(height, waist, neck)
 else:
-    usa_fat_percentage = athlete.usa_rasvaprosentti_nainen(height, waist, hips, neck)
+    usa_fat_percentage = athlete.usa_rasvaprosentti_nainen(
+        height, waist, hips, neck)
 
-text_to_show = f'Suomalainen rasva-% on {fat_percentage} ja amerikkalainen rasva-% on {usa_fat_percentage}'
+text_to_show = f'Suomalainen rasva-% on {fat_percentage} ja amerikkalainen rasva% on {usa_fat_percentage}'
 print(text_to_show)
 
-#TODO: Save user information to a file -> Serialization
-#TODO: Read it from a JSON file
-#TODO: Add date of weighing to kuntoilija-class
+print('nimi', athlete.nimi, 'paino', athlete.paino)
+
+athlete_data = [] # Empty list for all athlete data
+
+#Read previous athlete_data from disk
+with open('athlete_data.json', 'r') as file:
+    athlete_data = json.load(file)
+
+# A dictionary for single weighing of an athlete
+athlete_data_row = {'nimi': athlete.nimi, 'pituus': athlete.pituus, 'paino': athlete.paino,
+                'ika': athlete.ika, 'sukupuoli': athlete.sukupuoli, 'pvm': athlete.punnitus_paiva}
+
+# Add a new data row to the athlete_data list
+athlete_data.append(athlete_data_row)
+
+# SAVE DATA TO A FILE
+
+with open('athlete_data.json', 'w') as file:
+    json.dump(athlete_data, file)
+
+# TODO: Read it from a JSON file
