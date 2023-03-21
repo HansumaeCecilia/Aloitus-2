269b8ee956c677a530d67971803bcd1b349e8ee7
HansumaeCecilia <48033@edu.raseko.fi>
2023-03-21 13:45:03 +0200

    Removed unnessecary code

--- test_questions.py
+++ test_questions.py
@@ -3,21 +3,6 @@
 
 import questions
 
-# Test if conversion to integer works as expected
-
-# def test_ask_user_integer(monkeypatch): # Simulate user input using Monkeypatch library
-#     user_input = '100'
-#     monkeypatch.setattr('builtins.input', lambda _: user_input)
-#     question = questions.Question('Anna kokonaisluku: ')
-#     assert question.ask_user_integer(False) == (100, 'OK', 0, 'Conversion successful')
-
-# # Test an error condition when user adds a unit to a number
-# def test_ask_user_integer2(monkeypatch):
-#     user_input = '100v'
-#     monkeypatch.setattr('builtins.input', lambda _: user_input)
-#     question = questions.Question('Anna kokonaisluku: ')
-#     assert question.ask_user_integer(False) == (0, 'Error', 1, "invalid literal for int() with base 10: '100v'")
-
 # Test static conversion method on integer
 def test_static_ask_user_integer(monkeypatch):
     user_input = '100'
